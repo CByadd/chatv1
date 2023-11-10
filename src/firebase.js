00d10417008e23f69e5043f3a515c369071f2c38
Srinivash Dev <srinnivassh@gmail.com>
2023-11-10 17:31:35 +0530

    1

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyApHIebC99r8tPyAP8xuGaq_mzV-frBz7g",
+    authDomain: "chatapp-f18fb.firebaseapp.com",
+    projectId: "chatapp-f18fb",
+    storageBucket: "chatapp-f18fb.appspot.com",
+    messagingSenderId: "40354498394",
+    appId: "1:40354498394:web:f8e100af8e755cc28c1563",
+    measurementId: "G-8XMFJDR28H"
+  };
